cc7098c64d0b0d16e8cb3b2435f71d9baa67276d
antowkas <antonsokolov.ac@gmail.com>
2024-11-28 17:17:40 +0300

    Adding new feature Добавили новую функцию

--- main.py
+++ main.py
@@ -2,5 +2,10 @@ def print_hi(name):
     print(f'Привет, {name}!')
 
 
+def print_description():
+    print("Описание: Работаем над новым проектом")
+
+
 if __name__ == '__main__':
     print_hi('Антон')
+    print_description()
