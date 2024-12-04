09460c657102f5e5cd57003c51ea313ffaf13cf5
Alex <alex.surikov@inbox.ru>
2024-12-04 20:44:12 +0300

    Add changing 1 попытка смены статуса

--- add_task.py
+++ add_task.py
@@ -18,4 +18,20 @@ def adding_task():
     print('Задача успешно добавлена!')
 
 
+def changing():
+    con = sqlite3.connect("todo_list.db")
+    cur = con.cursor()
+
+    task_id = str(input("Введите id задачи: "))
+    new_status = str(input("Новой статус: "))
+
+    res = cur.execute(f"""UPDATE task
+                    SET task_status = '{new_status}'
+                    WHERE task_id = '{task_id}'""")
+
+    con.commit()
+    cur.close()
+    con.close()
+
+
 adding_task()
